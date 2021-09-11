2bef55337e48d875cda1c3506a5850b913c99806
Alex <alexbachurin91@gmail.com>
2021-09-11 19:07:27 +0300

    add to list functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaTrash } from 'react-icons/fa'
 import { AiFillEdit } from 'react-icons/ai'
 function App() {
   //setup list to store our items
-  const [list, setList] = useState([])
+  const [list, setList] = useState([]);
+  //store userinput
+  const [userInput, setuserInput] = useState('');
+
+  //on input change
+  const inputHandler = (e) => {
+    setuserInput(e.target.value);
+  }
+  //submit
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //add user input in our list in state
+    setList((list) => {
+      return [...list, userInput]
+    })
+    //clear input
+    setuserInput('');
+  }
+
   return (
     <section className='section-center'>
-      <form action="" className="post-form">
+      <form onSubmit={submitHandler} action="" className="post-form">
         <h3>Post here!</h3>
         <div className="form-control">
-          <input type="text" className="post" placeholder='e.g. Bunny' />
+          <input value={userInput} onChange={inputHandler} type="text" className="post" placeholder='e.g. Bunny' />
           <button type='submit' className='submit-btn'>submit </button>
         </div>
       </form>
