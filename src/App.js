b609d5ffa2a21975dee9fe7ead577afdfb32aec7
Alex <alexbachurin91@gmail.com>
2021-09-11 18:53:42 +0300

    display list functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaTrash } from 'react-icons/fa'
 import { AiFillEdit } from 'react-icons/ai'
 function App() {
+  //setup list to store our items
+  const [list, setList] = useState([])
   return (
     <section className='section-center'>
       <form action="" className="post-form">
@@ -13,20 +15,19 @@ function App() {
       </form>
       <div className="post-container">
         <div className="post-list">
-          <article className="post-item">
-            <p className="title">Nails</p>
-            <div className="btn-container">
-              <button type='button' className='edit-btn'><AiFillEdit /></button>
-              <button className="delete-btn" type='button'><FaTrash /></button>
-            </div>
-          </article>
-          <article className="post-item">
-            <p className="title">Fluffs</p>
-            <div className="btn-container">
-              <button type='button' className='edit-btn'><AiFillEdit /></button>
-              <button className="delete-btn" type='button'><FaTrash /></button>
-            </div>
-          </article>
+          {/* display list */}
+          {list.map(item => {
+            return (
+              <article key={new Date().getTime() + Math.random()} className="post-item">
+                <p className="title">{item}</p>
+                <div className="btn-container">
+                  <button type='button' className='edit-btn'><AiFillEdit /></button>
+                  <button className="delete-btn" type='button'><FaTrash /></button>
+                </div>
+              </article>
+            )
+          })}
+
           <button type='button' className='clear-btn'>clear All</button>
         </div>
       </div>
